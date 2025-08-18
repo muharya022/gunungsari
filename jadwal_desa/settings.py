9ae3f4ee975e1849a411faedf87649fb86f8e834
muharya022 <muhammadaryaalfajar@gmail.com>
2025-08-18 18:11:29 +0700

    puuuuu

--- jadwal_desa/settings.py
+++ jadwal_desa/settings.py
@@ -97,24 +97,11 @@ WSGI_APPLICATION = 'jadwal_desa.wsgi.application'
 # }
 
 
-# if os.environ.get("DATABASE_URL"):
-#     DATABASES = {
-#         "default": dj_database_url.config(
-#             conn_max_age=600,
-#             ssl_require=True
-#         )
-#     }
-# else:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.sqlite3",
-#             "NAME": BASE_DIR / "db.sqlite3",
-#         }
-#     }
+from decouple import config
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
+        default=config('DATABASE_URL'),
         conn_max_age=600,
         ssl_require=True
     )
