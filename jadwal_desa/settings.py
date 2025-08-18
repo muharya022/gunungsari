e1cbfd21a777f8d405f010b0b9c46b95c3d6ae66
muharya022 <muhammadaryaalfajar@gmail.com>
2025-08-18 13:28:00 +0700

    push2

--- jadwal_desa/settings.py
+++ jadwal_desa/settings.py
@@ -91,14 +91,20 @@ WSGI_APPLICATION = 'jadwal_desa.wsgi.application'
 # }
 
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://username:password@host:port/dbname',
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
-
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.config(
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
