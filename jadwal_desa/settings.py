1cef15be945226bb5192cc149b22e5f91073dafc
muharya022 <muhammadaryaalfajar@gmail.com>
2025-08-18 12:32:32 +0700

    puhs

--- jadwal_desa/settings.py
+++ jadwal_desa/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-qk7$ebo27&0gyc*6tw67=$vl9qfh%8j^i@ms0wc$px!h7i08gu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com', 'gunungsari.biz.id']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com', 'gunungsari.biz.id', "gunungsari-production.up.railway.app"]
 
 
 # Application definition
